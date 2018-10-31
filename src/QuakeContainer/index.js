4b5ac94bdb6e9219dd3f061ae6a4dac3acb22d6f
Ben <WBHankins93@Bens-MacBook-Air.local>
2018-10-31 11:58:34 -0500

    added quake and map container

---
+++ src/QuakeContainer/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class quakeContainer extends Component {
+
+  render() {
+    return(
+      <h1>IM ALIVEEEEEEEEEEEEE</h1>
+    );
+  }
+}
+
+export default quakeContainer;
